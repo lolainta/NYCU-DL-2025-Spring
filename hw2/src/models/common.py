0216407d6081e8210b579f7be81729ab86b007de
LolaInTa Chi <lolainta@gmail.com>
2025-03-23 15:34:17 +0800

    docs: dyt2d & bibtex cite not arxiv

--- hw2/src/models/common.py
+++ hw2/src/models/common.py
@@ -1,9 +1,28 @@
+import torch
 from torch import nn
+
+
+class DyT2d(nn.Module):
+    def __init__(self, num_features):
+        super().__init__()
+        self.alpha = nn.Parameter(torch.ones(num_features))
+        self.beta = nn.Parameter(torch.zeros(num_features))
+        self.gamma = nn.Parameter(torch.ones(num_features))
+
+    def forward(self, x):
+        B, C, H, W = x.size()
+        x = x.view(B, C, -1)
+        x = x * self.alpha.view(1, C, 1)
+        x = torch.tanh(x)
+        x = x * self.gamma.view(1, C, 1)
+        x = x + self.beta.view(1, C, 1)
+        x = x.view(B, C, H, W)
+        return x
 
 
 class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
-        super(DoubleConv, self).__init__()
+        super().__init__()
         self.double_conv = nn.Sequential(
             nn.Conv2d(
                 in_channels,
@@ -13,12 +32,14 @@ class DoubleConv(nn.Module):
                 padding=1,
                 bias=False,
             ),
-            nn.BatchNorm2d(out_channels),
+            # nn.BatchNorm2d(out_channels),
+            DyT2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(
                 out_channels, out_channels, kernel_size=3, padding="same", bias=False
             ),
-            nn.BatchNorm2d(out_channels),
+            # nn.BatchNorm2d(out_channels),
+            DyT2d(out_channels),
             nn.ReLU(inplace=True),
         )
 
